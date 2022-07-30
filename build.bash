4d04c19fb271541f2a9de1a09b30c5d62997cbfc
Marco Neumann <marco@crepererum.net>
2022-07-30 16:18:06 +0200

    `.desktop` support

--- build.bash
+++ build.bash
@@ -12,3 +12,16 @@ install -D lsb-release "${FLATPAK_DEST}/etc/lsb-release"
 pushd "$FLATPAK_DEST"
 bsdtar -Oxf "${FLATPAK_BUILDER_BUILDDIR}/Breitbandmessung-linux.deb" 'data.tar.*' |
     bsdtar -xf -
+
+# /usr/share => /share
+cp -R usr/* .
+rm -rf usr
+
+# .desktop file
+mv share/applications/breitbandmessung.desktop "share/applications/$FLATPAK_ID.desktop"
+desktop-file-edit --set-key=Exec --set-value=breitbandmessung "share/applications/$FLATPAK_ID.desktop"
+desktop-file-edit --set-key=Icon --set-value="$FLATPAK_ID" "share/applications/$FLATPAK_ID.desktop"
+
+# rename icons and remove large sizes
+find share/icons -name 'breitbandmessung.*' -path '*1024*' -delete
+find share/icons -name 'breitbandmessung.*' -exec rename -v breitbandmessung "$FLATPAK_ID" {} \;
