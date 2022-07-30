ea4d73aa5912fc51fc312fe786d100d26fa99316
Marco Neumann <marco@crepererum.net>
2022-07-30 15:47:17 +0200

    simplify system mocking

--- build.bash
+++ build.bash
@@ -2,22 +2,13 @@
 
 set -euo pipefail
 
-# install asar (later needed for application patching)
-npm install "asar@3.2.0" --offline
-
 # install run script
 install -Dm0755 run.bash "${FLATPAK_DEST}/bin/breitbandmessung"
+
+# lsb-release so that Breitbandmessung thinks is running on a normal system
+install -D lsb-release "${FLATPAK_DEST}/etc/lsb-release"
 
 # unpack debian package
 pushd "$FLATPAK_DEST"
 bsdtar -Oxf "${FLATPAK_BUILDER_BUILDDIR}/Breitbandmessung-linux.deb" 'data.tar.*' |
     bsdtar -xf -
-
-# path application to ignore that it is not running on debian
-pushd opt/Breitbandmessung/resources
-"${FLATPAK_BUILDER_BUILDDIR}/node_modules/asar/bin/asar.js" e app.asar unpacked/
-sed -i 's/allowed:!1/allowed:!0/g' unpacked/build/static/js/*.js
-sed -i 's/case"linux":/case"linux":break;/g' unpacked/build/static/js/*.js
-"${FLATPAK_BUILDER_BUILDDIR}/node_modules/asar/bin/asar.js" p unpacked/ app.asar
-rm -rf unpacked/
-
